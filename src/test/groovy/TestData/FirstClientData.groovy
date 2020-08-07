39cb2769d9fec5d6151df393bcf0f022df6c3e9f
StephenG98 <txsteven@gmail.com>
2020-08-06 20:31:07 -0500

    added some comments

--- src/test/groovy/TestData/FirstClientData.groovy
+++ src/test/groovy/TestData/FirstClientData.groovy
@@ -1,6 +1,8 @@
 package TestData
 
-class FirstClientData { // to really scale this class can extends an Abstract class or Interface
+// This can be scaled to handle different clients with minor differences in functionality by extending an
+//Abstract class or Interface
+class FirstClientData {
 
     private final String accountNumber = "1234"
     private final int APR = 35
@@ -15,6 +17,7 @@ class FirstClientData { // to really scale this class can extends an Abstract cl
         return APR
     }
 
+    // not using these methods in the definitions but it's the same principle as getAPR()
     int getRemainingCreditLimit() {
         return remainingCreditLimit
     }
